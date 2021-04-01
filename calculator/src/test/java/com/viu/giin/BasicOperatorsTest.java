98588fe2950778094d63a5c61a307cf10693aa24
Eduardo Zamudio <eduardozamudio@gmail.com>
2021-04-01 20:03:20 -0300

    Add maven

---
+++ calculator/src/test/java/com/viu/giin/BasicOperatorsTest.java
@@ -0,0 +1,11 @@
+package com.viu.giin;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ OperatorsTest_substractionDoubles_Test.class, OperatorsTest_sumIntegers_Test.class })
+public class BasicOperatorsTest {
+
+}
---
+++ calculator/src/test/java/com/viu/giin/OperatorsTest.java
@@ -0,0 +1,27 @@
+package com.viu.giin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+//import calculator.Operators;
+
+public class OperatorsTest {
+	
+	private Operators operator;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.operator = new Operators();
+	}
+
+	@Test
+	public void testSum() {
+		int a = 33;
+		int b = 22;
+		
+		assertEquals(55, this.operator.sum(a, b));
+	}
+
+}
